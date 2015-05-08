d3ec2386ccf4810c43d24ff04e739f2560f1047a
Noah J Anderson <noah.j.anderson@gmail.com>
2015-05-08 00:17:23 -0700

    More Comits

--- sqlsat/sqlsat/Schedule.sql
+++ sqlsat/sqlsat/Schedule.sql
@@ -4,6 +4,7 @@
     [ActorId] INT NOT NULL, 
     [LocationId] INT NOT NULL, 
     [Time] DATETIME NOT NULL, 
+    [EndTime] AS DATEADD( MINUTE, 60, [Time])
     PRIMARY KEY ([ClassId], [ActorId], [LocationId], [Time]), 
     CONSTRAINT [FK_Schedule_ClassId] FOREIGN KEY ([ClassId]) REFERENCES [Class]([ClassId]), 
     CONSTRAINT [FK_Schedule_Actor] FOREIGN KEY ([ActorId]) REFERENCES [Actor]([ActorId]),
